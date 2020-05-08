383aa4ea1b89c291dae0f5009ae91e3d20dccb49
ironnysh <63248335+ironnysh@users.noreply.github.com>
2020-05-08 10:32:58 +0200

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,42 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss")
+const pluginNavigation = require("@11ty/eleventy-navigation")
+const parseTransform = require("./src/assets/js/figcaption.js");
+
+module.exports = function (eleventyConfig) {
+    // Plugins
+    eleventyConfig.addPlugin(pluginRss)
+    eleventyConfig.addPlugin(pluginNavigation)
+
+    eleventyConfig.setDataDeepMerge(true);
+
+    // Add filters to Nunjucks
+    eleventyConfig.addFilter("squash", require("./src/_filters/squash.js"));
+    eleventyConfig.addFilter("hebrewDate", (dateObj) => {
+        const options = { weekday: "long", year: "numeric", month: "long", day: "2-digit" };
+        return new Date(dateObj).toLocaleDateString("de-DE", options);
+    });
+    
+    // Layouts
+    eleventyConfig.addLayoutAlias("layouts/base", "base.njk")
+    eleventyConfig.addLayoutAlias("layouts/page", "page.njk")
+    eleventyConfig.addLayoutAlias("layouts/links", "links.njk")
+    eleventyConfig.addLayoutAlias("layouts/thoughts", "thoughts.njk")
+
+    // Figcaption
+    eleventyConfig.addTransform("parse", parseTransform);
+
+    // Pass-through Files
+    eleventyConfig.addPassthroughCopy("src/assets")
+
+// Base eleventyConfig
+return {
+    dir: {
+        input: "src",
+        layouts: "_includes/layouts"
+    },
+    templateFormats: ["njk", "md", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    passthroughFileCopy: "true"
+    }
+}
