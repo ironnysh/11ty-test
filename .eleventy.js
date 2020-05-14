6580276114685b87d3c77e7f2cd251669fc86fac
ironnysh <63248335+ironnysh@users.noreply.github.com>
2020-05-14 14:14:30 +0200

    updates from latest version

--- .eleventy.js
+++ .eleventy.js
@@ -3,45 +3,52 @@ const pluginNavigation = require("@11ty/eleventy-navigation")
 const parseTransform = require("./src/assets/js/figcaption.js");
 
 module.exports = function (eleventyConfig) {
-    // Plugins
-    eleventyConfig.addPlugin(pluginRss)
-    eleventyConfig.addPlugin(pluginNavigation)
+  // Plugins
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(pluginNavigation)
 
-    eleventyConfig.setDataDeepMerge(true);
+  eleventyConfig.setDataDeepMerge(true);
 
-    // Collection
-  eleventyConfig.addCollection("thoughts", function(collection) {
+  // Collection
+  eleventyConfig.addCollection("thoughts", function (collection) {
     return collection.getFilteredByGlob("./src/thoughts/*.md");
   });
-    
-    // Filters
-    eleventyConfig.addFilter("squash", require("./src/_filters/squash.js"));
-    eleventyConfig.addFilter("hebrewDate", (dateObj) => {
-        const options = { year: "numeric", month: "long", day: "2-digit" };
-        return new Date(dateObj).toLocaleDateString("he-IL", options);
-    });
 
-    // Layouts
-    eleventyConfig.addLayoutAlias("layouts/base", "base.njk")
-    eleventyConfig.addLayoutAlias("layouts/page", "page.njk")
-    eleventyConfig.addLayoutAlias("layouts/links", "links.njk")
-    eleventyConfig.addLayoutAlias("layouts/thoughts", "thoughts.njk")
+  // Filters
+  eleventyConfig.addFilter("squash", require("./src/_filters/squash.js"));
+  eleventyConfig.addFilter('displayDate', function (date) {
+    return new Date(date).toLocaleDateString('he-IL', {
+      month: 'long',
+      year: 'numeric'
+    })
+  })
 
-    // Figcaption
-    eleventyConfig.addTransform("parse", parseTransform);
+  eleventyConfig.addFilter("hebrewDate", (dateObj) => {
+    const options = { year: "numeric", month: "long" };
+    return new Date(dateObj).toLocaleDateString("he-IL", options);
+  });
 
-    // Pass-through Files
-    eleventyConfig.addPassthroughCopy("src/assets")
+  // Layouts
+  eleventyConfig.addLayoutAlias("layouts/base", "base.njk")
+  eleventyConfig.addLayoutAlias("layouts/page", "page.njk")
+  eleventyConfig.addLayoutAlias("layouts/links", "links.njk")
+  eleventyConfig.addLayoutAlias("layouts/thoughts", "thoughts.njk")
 
-// Base eleventyConfig
-return {
+  // Figcaption
+  eleventyConfig.addTransform("parse", parseTransform);
+
+  // Pass-through Files
+  eleventyConfig.addPassthroughCopy("src/assets")
+
+  // Base eleventyConfig
+  return {
     dir: {
-        input: "src",
-        layouts: "_includes/layouts"
+      input: "src",
+      layouts: "_includes/layouts"
     },
     templateFormats: ["njk", "md", "html"],
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk",
     passthroughFileCopy: "true"
-    }
+  }
 }
